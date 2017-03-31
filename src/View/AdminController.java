718e7915358efeed0ae37fdf97d48f7bf4b7fef1
pkanukollu <pkanukollu>
2017-03-31 15:55:02 -0400

    admin controller

---
+++ src/View/AdminController.java
@@ -0,0 +1,19 @@
+package View;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class AdminController {
+	@FXML Button logout; 
+	@FXML Button create;
+	@FXML Button delete;
+	@FXML Button list;
+	@FXML Button quit;
+	@FXML TextField username;
+	public void start()
+    {
+	 
+    }
+	
+}
--- src/View/LoginController.java
+++ src/View/LoginController.java
@@ -1,5 +1,7 @@
 package View;
 import java.io.IOException;
+import java.util.LinkedList;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
